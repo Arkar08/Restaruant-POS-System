162fb3e98d781571b8d79a7c5847a834be271804
uiux@okdollar frontend <uiux@okdollar.com>
2024-12-20 16:49:04 +0630

    action button

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PaymentComponent } from "../../components/payment/payment.component";
 import {MatCardModule} from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-orders',
-  imports: [PaymentComponent,MatCardModule],
+  imports: [PaymentComponent,MatCardModule,MatButtonModule,MatIconModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
