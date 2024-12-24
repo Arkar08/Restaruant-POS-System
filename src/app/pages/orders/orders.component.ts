9462e03d8c0c41d11b8dd68e8ae62f854051a9c7
uiux@okdollar frontend <uiux@okdollar.com>
2024-12-24 11:59:00 +0630

    carousel

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PaymentComponent } from "../../components/payment/payment.component";
 import {MatCardModule} from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
 import { OrderCardComponent } from "../../components/order-card/order-card.component";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-orders',
-  imports: [PaymentComponent, MatCardModule, MatButtonModule, MatIconModule, OrderCardComponent],
+  imports: [PaymentComponent, MatCardModule, MatButtonModule, MatIconModule, OrderCardComponent,NgFor],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  styleUrl: './orders.component.css',
 })
-export class OrdersComponent {
-
+export class OrdersComponent implements OnInit {
+  fruits = ["Apple", "Banana", "Orange", "Grape", "Mango",'arkar','mama','mgmg'];
+    constructor(){}
+    ngOnInit(): void {
+        
+    }
+    currentIndex:number = 0;
+    clickIndex(step:any){
+      const totalslide = this.fruits.length;
+      this.currentIndex += step;
+      if (this.currentIndex < 0) {
+        this.currentIndex = totalslide - 1; 
+      } else if (this.currentIndex >= totalslide) {
+        this.currentIndex = 0; 
+      }      
+    }
 }
