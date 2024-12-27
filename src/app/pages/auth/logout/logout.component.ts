3a4792bdfda15a4de537612245309675a56117e3
uiux@okdollar frontend <uiux@okdollar.com>
2024-12-27 10:03:36 +0630

    logout design

--- src/app/pages/auth/logout/logout.component.ts
+++ src/app/pages/auth/logout/logout.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './logout.component.html',
   styleUrl: './logout.component.css'
 })
-export class LogoutComponent {
-
+export class LogoutComponent implements OnInit {
+  constructor(private router:Router) { }
+  ngOnInit(): void {
+  }
+  logout(){
+    this.router.navigate(['auth/login']);
+  }
+  cancel(){
+    this.router.navigate(['dashboard']);
+  }
 }
